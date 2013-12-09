7f23e40912d869fa06fa6ad31c9c35e15350cff6
Thibault Pierre <tib.pierre@gmail.com>
2013-12-09 16:55:48 +0100

    Moarrr cocktails

--- src/LaNetscouade/AfsyAventDemo/ApiBundle/Controller/DefaultController.php
+++ src/LaNetscouade/AfsyAventDemo/ApiBundle/Controller/DefaultController.php
@@ -23,40 +23,140 @@ class DefaultController extends Controller
 				'ingredients' => array(
 					array(
 						'name'     => 'Rhum Cubain',
-						'quantity' => '6cl'
+						'quantity' => '6cl',
 					),
 					array(
 						'name'     => 'Jus de citrons verts',
-						'quantity' => '3cl'
+						'quantity' => '3cl',
 					),
 					array(
 						'name'     => 'Feuilles de menthe',
-						'quantity' => '7'
+						'quantity' => '7',
 					),
 					array(
-						'name'     => 'Eau gazeuse'
+						'name'     => 'Eau gazeuse',
 					),
 					array(
 						'name'     => 'Sucre',
-						'quantity' => '2 cuillères à café'
+						'quantity' => '2 cuillères à café',
 					),
-				)
+				),
 			),
 			'Tequila Sunrise' => array(
 				'ingredients' => array(
 					array(
 						'name'     => 'Tequila',
-						'quantity' => '6cl'
+						'quantity' => '6cl',
 					),
 					array(
 						'name'     => 'Jus d\'oranges',
-						'quantity' => '12cl'
+						'quantity' => '12cl',
 					),
 					array(
 						'name'     => 'Sirop de grenadine',
-						'quantity' => '2cl'
-					)
-				)
+						'quantity' => '2cl',
+					),
+				),
+			),
+			'Bloody Mary' => array(
+				'ingredients' => array(
+					array(
+						'name'     => 'Vodka',
+						'quantity' => '4cl',
+					),
+					array(
+						'name'     => 'Jus de tomates',
+						'quantity' => '12cl',
+					),
+					array(
+						'name'     => 'Jus de citron',
+						'quantity' => '0.5cl',
+					),
+					array(
+						'name'     => 'Sauce worcestershire',
+						'quantity' => '0.5cl',
+					),
+					array(
+						'name'     => 'Tabasco',
+						'quantity' => '2 gouttes',
+					),
+					array(
+						'name'     => 'Sel de céleri',
+					),
+					array(
+						'name'     => 'Sel',
+					),
+					array(
+						'name'     => 'Poivre',
+					),
+				),
+			),
+			'Piña Colada' => array(
+				'ingredients' => array(
+					array(
+						'name'     => 'Rhum blanc',
+						'quantity' => '4cl',
+					),
+					array(
+						'name'     => 'Rhum ambré',
+						'quantity' => '2cl',
+					),
+					array(
+						'name'     => 'Jus d\'ananas',
+						'quantity' => '12cl',
+					),
+					array(
+						'name'     => 'Lait de coco',
+						'quantity' => '4cl',
+					),
+				),
+			),
+			'Cosmopolitan' => array(
+				'ingredients' => array(
+					array(
+						'name'     => 'Vodka',
+						'quantity' => '4cl',
+					),
+					array(
+						'name'     => 'Triple sec',
+						'quantity' => '2cl',
+					),
+					array(
+						'name'     => 'Jus de cranberry',
+						'quantity' => '2cl',
+					),
+					array(
+						'name'     => 'Jus de citron verts',
+						'quantity' => '1cl',
+					),
+				),
+			),
+			'Gin Tonic' => array(
+				'ingredients' => array(
+					array(
+						'name'     => 'Schweppes',
+					),
+					array(
+						'name'     => 'Gin',
+						'quantity' => '3cl',
+					),
+				),
+			),
+			'Monaco' => array(
+				'ingredients' => array(
+					array(
+						'name'     => 'Bière',
+						'quantity' => '15cl',
+					),
+					array(
+						'name'     => 'Sirop de grenadine',
+						'quantity' => '1cl',
+					),
+					array(
+						'name'     => 'Limonade',
+						'quantity' => '5cl',
+					),
+				),
 			),
 		);
     	$response = new JsonResponse();
